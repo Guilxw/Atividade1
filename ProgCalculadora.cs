96524eaf0dabfe3e1f69ceb7c3356ab573e69236
Guilxw <61891261+Guilxw@users.noreply.github.com>
2020-03-09 19:10:05 -0300

    Add files via upload

---
+++ ProgCalculadora.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ProgCalculadora
+{
+    class ProgCalculadora
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.Write("\n\n    5 + 2,9 = ");
+            calculadora c = new calculadora();
+            c.addNumero(5);
+            c.addNumero(2.9);
+            System.Console.WriteLine(c.somar());
+
+            System.Console.Write("\n\n    10 -5 -2 = ");
+            c.addNumero(10);
+            c.addNumero(5);
+            c.addNumero(2);
+            System.Console.WriteLine(c.subtrair());
+
+            System.Console.Write("\n\n    10 *2 *2 = ");
+            c.addNumero(10);
+            c.addNumero(2);
+            c.addNumero(2);
+            System.Console.WriteLine(c.multiplicar());
+
+            System.Console.Write("\n\n    6/2/2= ");
+            c.addNumero(6);
+            c.addNumero(2);
+            c.addNumero(2);
+            System.Console.WriteLine(c.Dividir());
+
+
+        }
+    }
+}
---
+++ calculadora.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+
+namespace ProgCalculadora
+{
+    class calculadora
+    {
+        private List<double> listNumero = new List<double>();
+
+        public void addNumero(double num)
+        {
+            this.listNumero.Add(num);
+        }
+
+        public double somar()
+        {
+            double resultado = 0;
+            foreach (double numero in this.listNumero)
+            {
+                resultado += numero;
+            }
+
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double subtrair()
+        {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double numero in this.listNumero)
+            {
+                resultado -= numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+        public double multiplicar()
+        {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double numero in this.listNumero)
+            {
+                resultado *= numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double Dividir()
+        {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double numero in this.listNumero)
+            {
+                resultado /= numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+    }
+}
